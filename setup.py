7efe5de3076db47476f0009746ab79c7722e6a66
IFcoltransG <i.r.galahad@gmail.com>
2020-09-06 23:39:05 +1200

    Bump minor version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="HashableDict",
-    version="0.1.2",
+    version="0.2.0",
     author="IFcoltransG",
     license="unlicense",
     author_email="IFcoltransG+PyPI@protonmail.ch",
