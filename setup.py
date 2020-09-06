50cc460a6b5318b62537ef6a03e17976fb9e48a1
IFcoltransG <i.r.galahad@gmail.com>
2020-09-06 21:47:12 +1200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="HashableDict",
-    version="0.1.0",
+    version="0.1.1",
     author="IFcoltransG",
     license="unlicense",
     author_email="IFcoltransG+PyPI@protonmail.ch",
